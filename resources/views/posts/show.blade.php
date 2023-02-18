cfd1b9273e04ce7df9bc8106dcf36c9927d3cebf
Youssef Tarek <youssft60@gmail.com>
2023-02-18 18:10:52 +0200

    inherting blade templates

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('pageName') Show Page @endsection
+
+@section('show')
+      <div class="card mt-5">
+            <div class="card-header">
+            Post Information
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+        <div class="card mt-5">
+            <div class="card-header">
+            Post Creator Information
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+@endsection
